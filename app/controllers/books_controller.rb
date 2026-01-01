13abb7e41b69d33746cf494c5ce61572b2501464
Bill Reed <bill.reed@babylist.com>
2026-01-01 15:16:09 -0500

    remove json handling from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
 
-  # GET /books or /books.json
+  # GET /books
   def index
     @books = Book.all
   end
 
-  # GET /books/1 or /books/1.json
+  # GET /books/1
   def show
   end
 
@@ -19,51 +19,37 @@ class BooksController < ApplicationController
   def edit
   end
 
-  # POST /books or /books.json
+  # POST /books
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, notice: "Book was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
+  # PATCH/PUT /books/1
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: "Book was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /books/1 or /books/1.json
+  # DELETE /books/1
   def destroy
     @book.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to books_path, notice: "Book was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    redirect_to books_path, notice: "Book was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def book_params
       params.expect(book: [ :title, :author, :total_pages, :current_page, :isbn, :status, :rating, :review_text, :started_at, :finished_at ])
     end
--- app/views/books/_book.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! book, :id, :title, :author, :total_pages, :current_page, :isbn, :status, :rating, :review_text, :started_at, :finished_at, :created_at, :updated_at
-json.url book_url(book, format: :json)
