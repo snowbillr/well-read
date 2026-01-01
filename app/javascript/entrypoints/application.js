6a86c1f283423e87d44eafd636701b71a6db6081
Bill Reed <bill.reed@babylist.com>
2026-01-01 13:53:08 -0500

    remove importmap

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,28 +1,15 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import * as Turbo from '@hotwired/turbo'
+import { Application } from "@hotwired/stimulus"
+import { registerControllers } from 'stimulus-vite-helpers'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+// Start Turbo
+Turbo.start()
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+// Start Stimulus
+const application = Application.start()
+const controllers = import.meta.glob('../controllers/*_controller.js', { eager: true })
+registerControllers(application, controllers)
 
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
