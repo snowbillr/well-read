79aa908c9ab7d83c3aa31bfefac96ce56ff4cfa9
Bill Reed <bill.reed@babylist.com>
2026-01-04 01:10:28 -0500

    nav bar link underlining

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_nav_link_class(path)
+    active = if path == root_path
+               current_page?(path)
+    else
+               request.path.start_with?(path)
+    end
+
+    base_classes = "text-sm font-bold font-serif text-deep-ink hover:opacity-70 transition pb-1 border-b-2"
+    active ? "#{base_classes} border-deep-ink" : "#{base_classes} border-transparent"
+  end
 end
